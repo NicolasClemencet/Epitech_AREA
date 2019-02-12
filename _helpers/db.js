55b4658990800a845f6c86d2c77dd9e93ea79edc
K0rolev <nicolas.clemencet@outlook.fr>
2019-02-12 13:25:05 +0100

    Nouveau dossier backend

--- _helpers/db.js
+++
@@ -1,8 +0,0 @@
-const config = require('config.json');
-const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI || config.connectionString);
-mongoose.Promise = global.Promise;
-
-module.exports = {
-    User: require('../users/users.model')
-};
